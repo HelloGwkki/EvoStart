533ac94e37986312ce0b697915bf1abcb71a3a9c
纯春茶 <53973238+HelloGwkki@users.noreply.github.com>
2022-09-12 00:58:59 +0000

    相对定位可能有问题？

--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@ console.log('%c Thanks for using! - Chuncha 💖', 'background-color: #000; colo
 console.log('-> Github: https://github.com/HelloGwkki/EvoStart <-');
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./sw.js').then(function (registration) {
+  navigator.serviceWorker.register('/sw.js').then(function (registration) {
     console.log('Registration successful, scope is:', registration.scope);
   }).catch(function (error) {
     console.log('Service Worker registration failed, error:', error);
